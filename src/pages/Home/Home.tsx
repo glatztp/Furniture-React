d12248947053db58a3a75cbca2b733819313635c
Gabriel Fellipe Glatz <ja36610@malwee.com.br>
2025-01-31 14:28:47 -0300

    Atualização da page Home com o Our Porducts Funcionando

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,41 +1,81 @@
 import style from "./Home.module.css";
 
 import { Footer } from "../../components/Footer/Footer";
-import Category from '../../components/Category_card/Category';
-import Produto from '../../components/Produto/produto';
+import Type from "../../components/Type/Type";
+import Produto from "../../components/Produto/produto";
 
-import imageCardCategory1 from '../../assets/Mask Group.svg'
+import imagetype1 from "../../assets/Mask Group.svg";
+import imgP from "../../assets/image 4.svg"
 
-interface Category {
+interface Type {
   name: string;
   imageUrl: string;
 }
 
-interface Product {
-  name: string;
-  price: number;
+interface Produto1 {
+  imageCardName: string;
+  imageCardDescription: string;
+  imageCardPrice: number;
   imageUrl: string;
-  description: string;
   BeforePrice?: number;
 }
 
 export const Home = () => {
-
-  const categories: Category[] = [
-    { name: "Dining", imageUrl: imageCardCategory1 },
-    { name: "Living", imageUrl: imageCardCategory1 },
-    { name: "Bedroom", imageUrl: imageCardCategory1 },
+  const type: Type[] = [
+    { name: "Dining", imageUrl: imagetype1 },
+    { name: "Living", imageUrl: imagetype1 },
+    { name: "Bedroom", imageUrl: imagetype1 },
   ];
 
-  const products: Product[] = [
-    { name: "Product 1", description: "Some description", price: 10.99, BeforePrice: 30, imageUrl: imageCardCategory1 },
-    { name: "Product 2", description: "Some description", price: 40.99, imageUrl: imageCardCategory1 },
-    { name: "Product 3", description: "Some description", price: 40.99, imageUrl: imageCardCategory1 },
-    { name: "Product 4", description: "Some description", price: 40.99, BeforePrice: 80, imageUrl: imageCardCategory1 },
-    { name: "Product 5", description: "Some description", price: 40.99, imageUrl: imageCardCategory1 },
-    { name: "Product 6", description: "Some description", price: 40.99, imageUrl: imageCardCategory1 },
-    { name: "Product 7", description: "Some description", price: 40.99, imageUrl: imageCardCategory1 },
-    { name: "Product 8", description: "Some description", price: 40.99, imageUrl: imageCardCategory1 },
+  const produtos: Produto1[] = [
+    {
+      imageCardName: "Prod 1",
+      imageCardDescription: "Desc 1",
+      imageCardPrice: 20.99,
+      imageUrl: imgP,
+    },
+    {
+      imageCardName: "Prod 2",
+      imageCardDescription: "Desc 2",
+      imageCardPrice: 29.90,
+      imageUrl: imgP,
+    },
+    {
+      imageCardName: "Prod 3",
+      imageCardDescription: "Desc 3",
+      imageCardPrice: 29.90,
+      imageUrl: imgP,
+    },
+    {
+      imageCardName: "Prod 4",
+      imageCardDescription: "Desc 4",
+      imageCardPrice: 29.90,
+      imageUrl: imgP,
+    },
+    {
+      imageCardName: "Prod 5",
+      imageCardDescription: "Desc 5",
+      imageCardPrice: 29.90,
+      imageUrl: imgP,
+    },
+    {
+      imageCardName: "Prod 6",
+      imageCardDescription: "Desc 6",
+      imageCardPrice: 29.90,
+      imageUrl: imgP,
+    },
+    {
+      imageCardName: "Prod 7",
+      imageCardDescription: "Desc 7",
+      imageCardPrice: 29.90,
+      imageUrl: imgP,
+    },
+    {
+      imageCardName: "Prod 8",
+      imageCardDescription: "Desc 8",
+      imageCardPrice: 29.90,
+      imageUrl: imgP,
+    },
   ];
 
   return (
@@ -59,41 +99,39 @@ export const Home = () => {
         <h1>Browse The Range</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 
-        {/* Categorias */}
         <div className={style.imageContainer}>
-          {categories.map((category, index) => (
+          {type.map((Type, index) => (
             <div key={index} className={style.imageCard}>
               <img
-                src={category.imageUrl}
-                alt={`Category ${index + 1}`}
+                src={Type.imageUrl}
+                alt={`Type ${index + 1}`}
                 className={style.image}
               />
-              <h1 className={style.imageName}>{category.name}</h1>
+              <h1 className={style.imageName}>{Type.name}</h1>
             </div>
           ))}
         </div>
-
-        {/* Produtos */}
-        <div className={style.our_products}>
+      </div>
+        <div className={style.products}>
           <h1 className={style.title}>Our Products</h1>
 
-          <div className={style.wrapper_products}>
-            {products.map((product, index) => (
+          <div className={style.c_products}>
+            {produtos.map((produtos, index) => (
               <Produto
                 key={index}
-                imageUrl={product.imageUrl}
-                name={product.name}
-                description={product.description}
-                price={product.price}
-                BeforePrice={product.BeforePrice}
+                imageUrl={produtos.imageUrl}
+                imageCardName={produtos.imageCardName}
+                imageCardDescription={produtos.imageCardDescription}
+                imageCardPrice={produtos.imageCardPrice}
               />
             ))}
           </div>
 
-          <a href="#" className={style.show_more}>Show More</a>
+          <a href="#" className={style.Show}>
+            Show More
+          </a>
         </div>
-
-      </div>
+      
 
       <Footer />
     </div>
