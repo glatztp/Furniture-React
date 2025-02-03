0c5417f2900a2b2d1ae9d1deca373bbfd82cdc69
Gabriel Fellipe Glatz <gabrielfellipeglatz@gmail.com>
2025-02-02 22:40:14 -0300

    Backgroud da page About

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,9 +1,11 @@
 import { Footer } from "../../components/Footer/Footer";
+import background from "../../assets/360_F_353839266_8yqhN0548cGxrl4VOxngsiJzDgrDHxjG.jpg"
 
 export function About() {
   return (
-    <div>About <Footer /></div>
-
-
+    <div>
+      <img src={background} className="w-full h-svh"></img>
+      <Footer />
+    </div>
   );
 }
